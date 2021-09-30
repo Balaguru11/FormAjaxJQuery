7141a005ac40c2125c2648bd7cb5ccff08357b3b
Balaguru <muraliguru99@gmail.com>
2021-09-30 18:37:54 +0530

    Shivin Code added

--- validation.js
+++ validation.js
@@ -57,6 +57,7 @@ $(document).ready(function() {
 
     // After Form Submitted Validation
   $("form").submit(function(event){
+    // $("input").trigger("input");
     var form_data=$("#signup").serializeArray();
     console.log(form_data);
     var error_free=true;
